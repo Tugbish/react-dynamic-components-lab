44b6fc80d4028db1c11187e6ad2de02ecc110208
Tugbish <tugba.kursun@yahoo.com.tr>
2021-12-02 22:54:44 +1100

    Complited

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,14 @@
-//your code here
+import React, { Component } from 'react';
+
+export default class Comment extends React.Component {
+   // let commentText;
+    render() {
+        return (
+        <div className='comment'> 
+            <h1>{this.props.commentText}</h1>;
+            
+        </div>
+        )
+            }
+}
+
